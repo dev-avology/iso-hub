18213d4b2fb361c8d6b1949560c20b460abadc29
dev-avology <devavology12@gmail.com>
2025-06-03 18:41:09 +0530

    added

--- src/pages/SecurePortal.tsx
+++ src/pages/SecurePortal.tsx
@@ -525,7 +525,7 @@ const SecurePortal: React.FC = () => {
               </div>
 
               <div>
-                <label className="block text-gray-300 mb-2">Select Form Pre-Application List</label>
+                <label className="block text-gray-300 mb-2">Select Form</label>
                 <select
                   name="formId"
                   value={formData.formId}
@@ -533,7 +533,7 @@ const SecurePortal: React.FC = () => {
                   className="w-full px-4 py-2 rounded bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-yellow-500"
                   required
                 >
-                  <option value="">Select a form</option>
+                  <option value="">Select From Pre-Application List</option>
                   {forms.map((form) => (
                     <option key={form.id} value={form.id}>
                       {form.business_dba}
